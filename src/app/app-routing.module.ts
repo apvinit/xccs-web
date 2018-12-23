e6944bfadbc259290d36dc86e34b3746b56bde2d
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-23 23:30:27 +0530

    Added default path to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
   {
-    path: '', redirectTo : '/about', pathMatch : 'full'
+    path: '', redirectTo : '/home' , pathMatch : 'full'
   },
   {
     path : '**' , component: PageNotFoundComponent
