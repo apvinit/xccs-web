f89edac850408282f6bde0a85e848c56146e1170
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-23 17:32:00 +0530

    Added material components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
     MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule
   ],
   exports : [
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
