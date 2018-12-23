10688f2c174c459c985078e090c4e5def1d149b7
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-23 19:47:34 +0530

    Fetched data from firesotre and displayed in template

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/firebase.service';
+import { Info } from '../../model/info';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private firebase : FirebaseService) { }
+  info : Info;
   ngOnInit() {
+    this.firebase.getInfo().subscribe((info : Info) => {
+      this.info = info;
+    });
   }
+
 
 }
