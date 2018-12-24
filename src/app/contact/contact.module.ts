4ac288be4b74ffb4056573e2b24ff35f7299b124
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-24 20:18:40 +0530

    Added material Module

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact/contact.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [ContactComponent],
   imports: [
     CommonModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    MaterialModule
   ]
 })
 export class ContactModule { }
