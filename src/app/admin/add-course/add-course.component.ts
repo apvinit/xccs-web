3a24fe1c984a2d8e67480e2c9101f9bc063b7b46
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-25 15:55:51 +0530

    Implemented Add Course Form

--- src/app/admin/add-course/add-course.component.ts
+++ src/app/admin/add-course/add-course.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/model/Course';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FirebaseService } from 'src/app/firebase.service';
 
 @Component({
   selector: 'app-add-course',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCourseComponent implements OnInit {
 
-  constructor() { }
+  courseForm = new FormGroup({
+    programme_code : new FormControl(''),
+    programme_name : new FormControl(''),
+    min_duration : new FormControl(''),
+    max_duration : new FormControl(''),
+    course_fee : new FormControl(''),
+    min_age : new FormControl(''),
+    max_age : new FormControl(''),
+    eligibility : new FormControl(''),
+    info : new FormControl(''),
+    description : new FormControl('')
+  })
+
+
+  course : Course;
+
+  constructor(private firebaseService : FirebaseService) { }
 
   ngOnInit() {
+  }
+
+  addCourse(){
+    this.course = this.courseForm.value;
+    this.firebaseService.addCourse(this.course);    
+    this.courseForm.reset()
   }
 
 }
