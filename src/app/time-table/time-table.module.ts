d31e9ac684e09fc29655e0008be7e69882260f42
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-25 16:30:38 +0530

    Added material module

--- src/app/time-table/time-table.module.ts
+++ src/app/time-table/time-table.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TimeTableRoutingModule } from './time-table-routing.module';
 import { TimeTableComponent } from './time-table/time-table.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [TimeTableComponent],
   imports: [
     CommonModule,
-    TimeTableRoutingModule
+    TimeTableRoutingModule,
+    MaterialModule
   ]
 })
 export class TimeTableModule { }
