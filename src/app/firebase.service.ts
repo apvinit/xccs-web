5dd08476757c93ce5186e2f53f253a27e75c671a
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-28 20:10:59 +0530

    Added file uploading method and file reference method

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore'
+import { AngularFireStorage, AngularFireUploadTask, AngularFireStorageReference } from '@angular/fire/storage';
 import { Observable } from 'rxjs';
 import { Info } from './model/info';
 import { Course } from './model/Course';
@@ -15,7 +16,9 @@ export class FirebaseService {
 
   info : Observable<Info>;
   
-  constructor(private firestore: AngularFirestore) { 
+  constructor(
+      private firestore: AngularFirestore,
+      private storage : AngularFireStorage ) { 
     this.infoRef = this.firestore.collection('about').doc('info');
   }
 
@@ -32,5 +35,16 @@ export class FirebaseService {
     this.firestore.collection<Course>('courses').add(course)
       .then((docref) => console.log("Added successfully with ref = " + docref.id))
       .catch((error) => console.log("Error Creating Document"));
+  }
+
+  getStorageRef(path): AngularFireStorageReference{
+    return this.storage.ref(path);
+  }
+
+  uploadFile(path, event) : AngularFireUploadTask{
+    const filePath = path;
+    const file = event.target.files[0];
+
+    return this.storage.upload(filePath, file);
   }
 }
