dceeeb1372c72f4869ed60b6713ab12bbb89fb3a
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-28 22:03:18 +0530

    Added method for adding time table

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -47,4 +47,14 @@ export class FirebaseService {
 
     return this.storage.upload(filePath, file);
   }
+
+  addTimeTable(id, content){ 
+    let timetableRef = this.firestore.collection('timetable');
+    timetableRef.doc(id.toUpperCase()).set({prog_name : content.prog_name});
+    timetableRef.doc(id.toUpperCase()).collection(content.timetable_type)
+                                      .doc(content.semester).set({ url : content.url})
+                                      .then((docref) => {console.log("Added successfully")})
+                                      .catch((error) => console.log("Error : Adding Timetable"));
+  }
+
 }
