613e4cde43bf776b434a126d388ca3b4f0153c53
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-29 18:50:34 +0530

    Implemented add event behaviour

--- src/app/admin/add-event/add-event.component.ts
+++ src/app/admin/add-event/add-event.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { FirebaseService } from 'src/app/firebase.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { AngularFireUploadTask } from '@angular/fire/storage';
+import { Event_ } from '../../model/event';
 
 @Component({
   selector: 'app-add-event',
@@ -7,9 +13,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
-  constructor() { }
+  eventForm = new FormGroup({
+    title : new FormControl(''),
+    subtitle : new FormControl(''),
+    description : new FormControl('')
+  });
+
+  uploading = null; uploaded = false;
+
+  // Variables
+  title : string; subtitle : string; description : string;
+  images : string[] = [];
+
+  uploadPercent: Observable<number>;
+  constructor(private firebaseService : FirebaseService) { }
 
   ngOnInit() {
+  }
+
+  uploadFile(event){
+    let files = event.target.files;
+    this.title = this.eventForm.get('title').value;
+    
+    for(let i = 0; i < files.length; i++){
+      let task : AngularFireUploadTask;
+      let file = files[i];
+      const filePath = 'Events/' + this.title + '/' + file.name;
+      const fileRef = this.firebaseService.getStorageRef(filePath);
+      task = this.firebaseService.uploadFile(filePath, file);
+      this.uploading = true;
+      task.percentageChanges().subscribe((value : number) => {
+        if(value == 100){
+          this.uploading = false;
+        }
+      })
+
+      // observe percentage changes
+      this.uploadPercent = task.percentageChanges();
+      // get notified when the download URL is available
+      task.snapshotChanges().pipe(
+          finalize(() => {
+            fileRef.getDownloadURL().subscribe((url) => {
+              this.images.push(url);
+              this.uploaded = true;
+            });
+            
+          })
+      )
+      .subscribe();
+
+    }
+  }
+
+  onSubmit(){
+    this.title = this.eventForm.get('title').value;
+    this.subtitle = this.eventForm.get('subtitle').value;
+    this.description = this.eventForm.get('description').value;
+
+    let event: Event_ = {
+      title : this.title,
+      subtitle : this.subtitle,
+      description : this.description,
+      thumbnail : this.images[0],
+      images : this.images
+    }
+    
+    this.firebaseService.addEvent(event);
+    
   }
 
 }
