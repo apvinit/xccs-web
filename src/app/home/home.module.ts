890b6d9b696b5c5175da44e961c07febbdcfcc60
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-29 19:55:54 +0530

    Created news component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NewsComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
