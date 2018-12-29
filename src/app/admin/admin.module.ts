9df593734650b4f8b39960cc0511dd2b3ebb925b
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-29 14:21:47 +0530

    Created add event component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,10 +6,11 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
 import { MaterialModule } from '../material.module';
 import { AddCourseComponent } from './add-course/add-course.component';
-import { AddTimeTableComponent } from './add-time-table/add-time-table.component'
+import { AddTimeTableComponent } from './add-time-table/add-time-table.component';
+import { AddEventComponent } from './add-event/add-event.component'
 
 @NgModule({
-  declarations: [AdminComponent, AddCourseComponent, AddTimeTableComponent],
+  declarations: [AdminComponent, AddCourseComponent, AddTimeTableComponent, AddEventComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
