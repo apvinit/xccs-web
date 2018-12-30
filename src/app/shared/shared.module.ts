6cd050359154e0cd4558d7f355853b6a6eb675db
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-30 11:22:58 +0530

    Imported material module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { MaterialModule } from '../material.module';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [FooterComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    MaterialModule
   ],
   exports : [
     FooterComponent
