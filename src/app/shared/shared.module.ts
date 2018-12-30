6924c53abf92a71856541041bf308014243438f6
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-30 11:17:17 +0530

    created a shared footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
+  ],
+  exports : [
+    FooterComponent
   ]
 })
 export class SharedModule { }
