e337992f23b7777c5d26e17432c6fa61283659c1
h4rdw1r3 <vinaugust6@gmail.com>
2018-12-31 11:31:28 +0530

    Added firebase config to production website

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDuIfvZhRrZ5KeIQS6spGSaWxZff23UFqg",
+    authDomain: "xccs-0516p.firebaseapp.com",
+    databaseURL: "https://xccs-0516p.firebaseio.com",
+    projectId: "xccs-0516p",
+    storageBucket: "xccs-0516p.appspot.com",
+    messagingSenderId: "360110804640"
+  } 
 };
