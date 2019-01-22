fa5dbed6cf883b66f1c97de6b2639c7a6921a417
h4rdw1r3 <vinaugust6@gmail.com>
2019-01-22 12:02:58 +0530

    Created routes for admin module

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { AddNewsComponent } from './add-news/add-news.component';
+import { AddEventComponent } from './add-event/add-event.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { AddTimeTableComponent } from './add-time-table/add-time-table.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
-    path: 'xccs-admin', component: AdminComponent
+    path: 'xccs-admin', component : AdminComponent,
+    children : [
+      {
+        path : '', component : DashboardComponent
+      },
+      {
+        path : 'news', component : AddNewsComponent
+      },
+      {
+        path : 'events', component : AddEventComponent
+      },
+      {
+        path : 'timetable', component : AddTimeTableComponent
+      },
+      {
+        path : 'courses', component : AddCourseComponent
+      }
+    ]
   }
 ];
 
