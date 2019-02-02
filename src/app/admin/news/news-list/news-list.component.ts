208e92d40a3c43e2569dc07c05d51daf1f25709e
h4rdw1r3 <vinaugust6@gmail.com>
2019-02-02 22:16:55 +0530

    Added edit method

--- src/app/admin/news/news-list/news-list.component.ts
+++ src/app/admin/news/news-list/news-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from 'src/app/firebase.service';
 import { Observable } from 'rxjs';
 import { News } from 'src/app/model/news';
+import { MatDialog } from '@angular/material';
+import { AddNewsComponent } from '../../add-news/add-news.component';
 
 @Component({
   selector: 'app-news-list',
@@ -9,12 +11,23 @@ import { News } from 'src/app/model/news';
   styleUrls: ['./news-list.component.scss']
 })
 export class NewsListComponent implements OnInit {
-
-  constructor(private firebaseService: FirebaseService) { }
+  constructor(
+    private firebaseService: FirebaseService,
+    private dialog: MatDialog
+  ) {}
 
   newsItems$: Observable<News[]>;
   ngOnInit() {
     this.newsItems$ = this.firebaseService.getNews();
+  }
+
+  edit(newsItem: News): void {
+    const editNewsDialogRef = this.dialog.open(AddNewsComponent, {
+      width: '400px',
+      data: { edit : 'true', newsItem: newsItem as News }
+    });
+
+    editNewsDialogRef.afterClosed().subscribe(result => console.log(result));
   }
 
   delete(id: string): void {
