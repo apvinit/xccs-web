9bcb2675e96e2b5d3ecf3a142dcdd2b333a6b9bc
h4rdw1r3 <vinaugust6@gmail.com>
2019-02-03 14:23:27 +0530

    Created a shared confirmation dialog component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,24 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { MaterialModule } from '../material.module';
 import { FooterComponent } from './footer/footer.component';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 
 @NgModule({
-  declarations: [FooterComponent],
+  declarations: [
+    FooterComponent,
+    ConfirmationDialogComponent
+  ],
   imports: [
     CommonModule,
     SharedRoutingModule,
     MaterialModule
   ],
   exports : [
-    FooterComponent
+    FooterComponent,
+    ConfirmationDialogComponent
+  ],
+  entryComponents: [
+    ConfirmationDialogComponent
   ]
 })
 export class SharedModule { }
