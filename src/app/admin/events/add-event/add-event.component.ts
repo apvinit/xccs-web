3cd909447721ea8a6bc65d5b02d6a20d35c2ac99
h4rdw1r3 <vinaugust6@gmail.com>
2019-02-12 21:16:32 +0530

    Fixed path bug and refactored  the file

--- src/app/admin/events/add-event/add-event.component.ts
+++ src/app/admin/events/add-event/add-event.component.ts
@@ -4,7 +4,7 @@ import { finalize } from 'rxjs/operators';
 import { FirebaseService } from 'src/app/firebase.service';
 import { FormGroup, FormControl } from '@angular/forms';
 import { AngularFireUploadTask } from '@angular/fire/storage';
-import { Event_ } from '../../model/event';
+import { Event_ } from '../../../model/event';
 
 @Component({
   selector: 'app-add-event',
@@ -12,74 +12,73 @@ import { Event_ } from '../../model/event';
   styleUrls: ['./add-event.component.scss']
 })
 export class AddEventComponent implements OnInit {
-
   eventForm = new FormGroup({
-    title : new FormControl(''),
-    subtitle : new FormControl(''),
-    description : new FormControl('')
+    title: new FormControl(''),
+    subtitle: new FormControl(''),
+    description: new FormControl('')
   });
 
-  uploading = null; uploaded = false;
+  uploading = null;
+  uploaded = false;
 
   // Variables
-  title : string; subtitle : string; description : string;
-  images : string[] = [];
+  title: string;
+  subtitle: string;
+  description: string;
+  images: string[] = [];
 
   uploadPercent: Observable<number>;
-  constructor(private firebaseService : FirebaseService) { }
+  constructor(private firebaseService: FirebaseService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  uploadFile(event){
-    let files = event.target.files;
+  uploadFile(event) {
+    const files = event.target.files;
     this.title = this.eventForm.get('title').value;
-    
-    for(let i = 0; i < files.length; i++){
-      let task : AngularFireUploadTask;
-      let file = files[i];
+
+    for (let i = 0; i < files.length; i++) {
+      let task: AngularFireUploadTask;
+      const file = files[i];
       const filePath = 'Events/' + this.title + '/' + file.name;
       const fileRef = this.firebaseService.getStorageRef(filePath);
       task = this.firebaseService.uploadFile(filePath, file);
       this.uploading = true;
-      task.percentageChanges().subscribe((value : number) => {
-        if(value == 100){
+      task.percentageChanges().subscribe((value: number) => {
+        if (value === 100) {
           this.uploading = false;
         }
-      })
+      });
 
       // observe percentage changes
       this.uploadPercent = task.percentageChanges();
       // get notified when the download URL is available
-      task.snapshotChanges().pipe(
+      task
+        .snapshotChanges()
+        .pipe(
           finalize(() => {
-            fileRef.getDownloadURL().subscribe((url) => {
+            fileRef.getDownloadURL().subscribe(url => {
               this.images.push(url);
               this.uploaded = true;
             });
-            
           })
-      )
-      .subscribe();
-
+        )
+        .subscribe();
     }
   }
 
-  onSubmit(){
+  onSubmit() {
     this.title = this.eventForm.get('title').value;
     this.subtitle = this.eventForm.get('subtitle').value;
     this.description = this.eventForm.get('description').value;
 
-    let event: Event_ = {
-      title : this.title,
-      subtitle : this.subtitle,
-      description : this.description,
-      thumbnail : this.images[0],
-      images : this.images
-    }
-    
-    this.firebaseService.addEvent(event);
-    
-  }
+    const event: Event_ = {
+      title: this.title,
+      subtitle: this.subtitle,
+      description: this.description,
+      thumbnail: this.images[0],
+      images: this.images
+    };
 
+    this.firebaseService.addEvent(event);
+  }
 }
