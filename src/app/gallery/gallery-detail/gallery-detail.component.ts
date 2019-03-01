ae18a8b016f04bbc24c274fc341c0301c1b1883f
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-01 23:44:56 +0530

    Create a component for events images details

---
+++ src/app/gallery/gallery-detail/gallery-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-detail',
+  templateUrl: './gallery-detail.component.html',
+  styleUrls: ['./gallery-detail.component.scss']
+})
+export class GalleryDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -5,9 +5,10 @@ import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryComponent } from './gallery/gallery.component';
 import { MaterialModule } from '../material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { GalleryDetailComponent } from './gallery-detail/gallery-detail.component';
 
 @NgModule({
-  declarations: [GalleryComponent],
+  declarations: [GalleryComponent, GalleryDetailComponent],
   imports: [
     CommonModule,
     GalleryRoutingModule,
