42c21c51fd2c10ef96871209092d49b2ef1ac1cc
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-01 23:31:53 +0530

    made changes a/c to the lint

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -7,15 +7,28 @@ import { Person } from 'src/app/model/person';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-  private prominentPersons: Person[] = [
-    { name: "Rector", description: "Rector, St. Xavier's School", imageUrl: "http://0516p.info/imp%20img/rec.jpg"},
-    { name: "Christu Savarirajan", description: "Principal, St. Xavier's School", imageUrl: "http://0516p.info/imp%20img/pr.jpg"},
-    { name: "Dr. Abhilash Nayak", description: "Regional Director, IGNOU", imageUrl: "http://0516p.info/imp%20img/rdPatna.jpg"},
-    { name: "Md. Shams Raza", description: "Programme Incharge 0516P", imageUrl: "http://0516p.info/imp%20img/sir.jpg"},
-  ]
-  ngOnInit() {
-  }
-
+  constructor() {}
+  public prominentPersons: Person[] = [
+    {
+      name: 'Rector',
+      description: 'Rector, St. Xavier\'s School',
+      imageUrl: 'http://0516p.info/imp%20img/rec.jpg'
+    },
+    {
+      name: 'Christu Savarirajan',
+      description: 'Principal, St. Xavier\'s School',
+      imageUrl: 'http://0516p.info/imp%20img/pr.jpg'
+    },
+    {
+      name: 'Dr. Abhilash Nayak',
+      description: 'Regional Director, IGNOU',
+      imageUrl: 'http://0516p.info/imp%20img/rdPatna.jpg'
+    },
+    {
+      name: 'Md. Shams Raza',
+      description: 'Programme Incharge 0516P',
+      imageUrl: 'http://0516p.info/imp%20img/sir.jpg'
+    }
+  ];
+  ngOnInit() {}
 }
