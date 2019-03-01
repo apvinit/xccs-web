71de288ea797a52af7a404eee6ccffaebc23a45b
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-01 20:21:11 +0530

    Removded hard coded prominent person items with ngFor directive

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from 'src/app/model/person';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  private prominentPersons: Person[] = [
+    { name: "Rector", description: "Rector, St. Xavier's School", imageUrl: "http://0516p.info/imp%20img/rec.jpg"},
+    { name: "Christu Savarirajan", description: "Principal, St. Xavier's School", imageUrl: "http://0516p.info/imp%20img/pr.jpg"},
+    { name: "Dr. Abhilash Nayak", description: "Regional Director, IGNOU", imageUrl: "http://0516p.info/imp%20img/rdPatna.jpg"},
+    { name: "Md. Shams Raza", description: "Programme Incharge 0516P", imageUrl: "http://0516p.info/imp%20img/sir.jpg"},
+  ]
   ngOnInit() {
   }
 
