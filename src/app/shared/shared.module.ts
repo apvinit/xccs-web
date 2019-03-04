aee4646b7708004dfb14435a219881313e3f0e20
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-04 21:01:34 +0530

    Add Reactive Forms module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { MaterialModule } from '../material.module';
@@ -16,6 +17,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     SharedRoutingModule,
     MaterialModule,
     FlexLayoutModule
