e429532a336742645dfd9f698babf3fd47ad29e8
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-04 17:11:24 +0530

    Removed unnecessary import statements

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
-import { AddCourseComponent } from './courses/add-course/add-course.component';
-import { AddEventComponent } from './events/add-event/add-event.component';
-import { AddNewsComponent } from './news/add-news/add-news.component';
-import { AddTimeTableComponent } from './time-table/add-time-table/add-time-table.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ManageNewsComponent } from './news/manage-news/manage-news.component';
 import { NewsListComponent } from './news/news-list/news-list.component';
