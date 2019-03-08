c065b01d3e6860e151241a9d06c04c4280d8036d
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-08 13:27:50 +0530

    Fixed gallery detail component not loading

--- src/app/gallery/gallery-routing.module.ts
+++ src/app/gallery/gallery-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: '', component: GalleryComponent
   },
   {
-    path: 'gallery/:event', component: GalleryDetailComponent
+    path: ':event', component: GalleryDetailComponent
   }
 ];
 
