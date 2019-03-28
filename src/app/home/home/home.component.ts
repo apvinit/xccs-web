952df96773088dce3b015eced98ca854b18af8ae
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-28 18:02:23 +0530

    Update imageUrl to secure host
    Earlier the host serving image was insecure. So changed the url to
    secure host.

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -12,22 +12,26 @@ export class HomeComponent implements OnInit {
     {
       name: 'Rector',
       description: 'Rector, St. Xavier\'s School',
-      imageUrl: 'http://0516p.info/imp%20img/rec.jpg'
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/xccs-0516p.appspot.com/o/staffs%2Frector.jpg?alt=media&token=b5fbf909-fc21-4e80-abab-6764ddff8917'
     },
     {
       name: 'Christu Savarirajan',
       description: 'Principal, St. Xavier\'s School',
-      imageUrl: 'http://0516p.info/imp%20img/pr.jpg'
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/xccs-0516p.appspot.com/o/staffs%2Fprincipal.jpg?alt=media&token=5ff50867-6587-4602-af0c-b6396f3aba6d'
     },
     {
       name: 'Dr. Abhilash Nayak',
       description: 'Regional Director, IGNOU',
-      imageUrl: 'http://0516p.info/imp%20img/rdPatna.jpg'
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/xccs-0516p.appspot.com/o/staffs%2FrdPatna.jpg?alt=media&token=1a8648f5-422b-4879-962f-7e07fd06ec51'
     },
     {
       name: 'Md. Shams Raza',
       description: 'Programme Incharge 0516P',
-      imageUrl: 'http://0516p.info/imp%20img/sir.jpg'
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/xccs-0516p.appspot.com/o/staffs%2Fcoordinator.jpg?alt=media&token=1204a420-2bbf-4efe-b397-1966d35cd8f1'
     }
   ];
   ngOnInit() {}
